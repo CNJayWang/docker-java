50fb4d485e59398ac3400dab3545fbb0060fefc6
marsqing <qing.gu@dianping.com>
2014-02-17 21:32:50 +0800

    -n container port is not Ports

--- src/main/java/com/kpelykh/docker/client/model/Container.java
+++ src/main/java/com/kpelykh/docker/client/model/Container.java
@@ -1,100 +1,134 @@
 package com.kpelykh.docker.client.model;
 
+import java.util.List;
+
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 
-import java.util.Arrays;
-
 /**
- *
+ * 
  * @author Konstantin Pelykh (kpelykh@gmail.com)
- *
+ * 
  */
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Container {
 
-    @JsonProperty("Id")
-    private String id;
+	@JsonProperty("Id")
+	private String id;
 
-    @JsonProperty("Command")
-    private String command;
+	@JsonProperty("Command")
+	private String command;
 
-    @JsonProperty("Image")
-    private String image;
+	@JsonProperty("Image")
+	private String image;
 
-    @JsonProperty("Created")
-    private long created;
+	@JsonProperty("Created")
+	private long created;
 
-    @JsonProperty("Status")
-    private String status;
+	@JsonProperty("Status")
+	private String status;
 
-    /* Example:
-    "Ports": {
-        "22/tcp": [
-            {
-                "HostIp": "0.0.0.0",
-                "HostPort": "8022"
-            }
-        ]
-    }
-    */
+	/*
+	 * Example: "Ports": { "22/tcp": [ { "HostIp": "0.0.0.0", "HostPort": "8022" } ] }
+	 */
 
-    @JsonProperty("Ports")    
-    public Ports ports;
+	@JsonProperty("Ports")
+	public List<Container.Port> ports;
 
-    @JsonProperty("SizeRw")
-    private int size;
+	@JsonProperty("SizeRw")
+	private int size;
 
-    @JsonProperty("SizeRootFs")
-    private int sizeRootFs;
+	@JsonProperty("SizeRootFs")
+	private int sizeRootFs;
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getCommand() {
-        return command;
-    }
+	public String getCommand() {
+		return command;
+	}
 
-    public String getImage() {
-        return image;
-    }
+	public String getImage() {
+		return image;
+	}
 
-    public long getCreated() {
-        return created;
-    }
+	public long getCreated() {
+		return created;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public Ports getPorts() {
-        return ports;
-    }
+	public List<Container.Port> getPorts() {
+		return ports;
+	}
 
-    public void setPorts(Ports ports) {
-        this.ports = ports;
-    }
+	public void setPorts(List<Container.Port> ports) {
+		this.ports = ports;
+	}
 
-    public int getSize() {
-        return size;
-    }
+	public int getSize() {
+		return size;
+	}
 
-    public int getSizeRootFs() {
-        return sizeRootFs;
-    }
+	public int getSizeRootFs() {
+		return sizeRootFs;
+	}
 
-    @Override
-    public String toString() {
-        return "Container{" +
-                "id='" + id + '\'' +
-                ", command='" + command + '\'' +
-                ", image='" + image + '\'' +
-                ", created=" + created +
-                ", status='" + status + '\'' +
-                ", ports=" + ports +
-                ", size=" + size +
-                ", sizeRootFs=" + sizeRootFs +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Container{" + "id='" + id + '\'' + ", command='" + command + '\'' + ", image='" + image + '\''
+		      + ", created=" + created + ", status='" + status + '\'' + ", ports=" + ports + ", size=" + size
+		      + ", sizeRootFs=" + sizeRootFs + '}';
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class Port {
+		@JsonProperty("PrivatePort")
+		private int privatePort;
+
+		@JsonProperty("IP")
+		private String ip;
+
+		@JsonProperty("PublicPort")
+		private int publicPort;
+
+		@JsonProperty("Type")
+		private String type;
+
+		public void setPrivatePort(int privatePort) {
+			this.privatePort = privatePort;
+		}
+
+		public void setIp(String ip) {
+			this.ip = ip;
+		}
+
+		public void setPublicPort(int publicPort) {
+			this.publicPort = publicPort;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public int getPrivatePort() {
+			return privatePort;
+		}
+
+		public String getIp() {
+			return ip;
+		}
+
+		public int getPublicPort() {
+			return publicPort;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+	}
 }
